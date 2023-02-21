e8df95780d54f9bbabb31bad51ebfd222b4fc418
pushpali8 <pgpiyali834@gmail.com>
2023-02-21 14:46:42 +0530

    first commit

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 import os
 from flask import  Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-import cv2
+import cv2#converting uploaded images in static folder to array and then changing the value so that it becomes black and white
 import numpy as np
 
 app = Flask(__name__)
 
 # Open and redirect to default upload webpage
-@app.route('/')
+@app.route('/')#to run the function in home page
 def load_form():
-    return render_template('upload.html')
+    return render_template('upload.html')# to show the output in html page
 
 
 # Function to upload image and redirect to new webpage
@@ -19,36 +19,37 @@ def upload_image():
     file = request.files['file']
     filename = secure_filename(file.filename)
 
-    file_data = make_grayscale(file.read())
+    file_data = make_grayscale(file.read())#passing image file to make_grayscale() where the image is converted into black and white and then stored in file_data
+    #to store BW image we need to open the place where the coloured image is stored and then put file_data variable over there
     with open(os.path.join('static/', filename),
-              'wb') as f:
+              'wb') as f:#wb-write binary
         f.write(file_data)
 
     display_message = 'Image successfully uploaded and displayed below'
-    return render_template('upload.html', filename=filename, message = display_message)
+    return render_template('upload.html', filename=filename, message = display_message)#only upload image and show msg
 
 
 
 def make_grayscale(input_image):
 
-    image_array = np.fromstring(input_image, dtype='uint8')
+    image_array = np.fromstring(input_image, dtype='uint8')#to convert coloured image into pixel single-line array whose datatype is uint8
     print('Image Array:',image_array)
 
     # decode the array into an image
-    decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+    decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)#converts single array into matrix format  i.e proper RGB format array with 3 columns
     print('Decode values of Image:', decode_array_to_img)
 
     # Make grayscale
-    converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)
-    status, output_image = cv2.imencode('.PNG', converted_gray_img)
+    converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)#converts image to BW
+    status, output_image = cv2.imencode('.PNG', converted_gray_img)#converts image to original format
     print('Status:',status)
 
-    return output_image
+    return output_image#returned to file_data variable
 
 
 @app.route('/display/<filename>')
 def display_image(filename):
-    return redirect(url_for('static', filename=filename))
+    return redirect(url_for('static', filename=filename))#upload image from static folder to the same page
 
 
 
